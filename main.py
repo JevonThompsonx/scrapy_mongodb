2e129fb87e9b0dc9ff71460be185b49baab510d3
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-08-29 14:46:05 -0700

    Fixed errors

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import httpx 
+"""
+My first project with python using scrappy to web scrape
+
+The goal will be to web scrape the arch user repository for package information
+"""
+from pprint import pprint
+import httpx
 import scrapy
-from pprint import pprint 
 
 pprint(dir(httpx))
-pprint(dir(hhtpx))
+pprint(dir(scrapy))
