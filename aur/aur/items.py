45e32dee1e641a6345816639df5f10f65dcb5d1b
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-08-31 10:17:40 -0700

    Created aur package items base
    Using scrapy, I created a base model for what the scraped aur package should look like

--- aur/aur/items.py
+++ aur/aur/items.py
@@ -6,7 +6,11 @@
 import scrapy
 
 
-class AurItem(scrapy.Item):
+class aurPackage(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    currentVersion = scrapy.Field()
+    description = scrapy.Field()
+    maintainer = scrapy.Field()
+    lastUpdated = scrapy.Field()
