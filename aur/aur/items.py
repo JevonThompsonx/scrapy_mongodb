59c9409fdea2809bf2f26a5bc5f8b71aaa2dce77
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-08-31 13:57:09 -0400

    Updated w/ css selectors
    These might require more testing but for my own sake I'm documenting what I think are the correct css selectors for the aurPackage attributes and will get back to this later

--- aur/aur/items.py
+++ aur/aur/items.py
@@ -6,11 +6,28 @@
 import scrapy
 
 
-class aurPackage(scrapy.Item):
+class AurPackage(scrapy.Item):
+    """Class created to model the web scraped aur package
+    
+    `scrapy.Field() is used to create a temporary placeholder 
+    it's really a dict alias. The expected values are below`
+
+    Attributes
+    ----------
+    name:str #css selector
+    description:str #css selector
+    maintainer:str #css selector
+    lastUpdated:str #css selector
+    """
     # define the fields for your item here like:
     # name = scrapy.Field()
     name = scrapy.Field()
+    # element.css('td:nth-child(1)> a::text')
     currentVersion = scrapy.Field()
+    #element.css('td:nth-child(2)::text')
     description = scrapy.Field()
+    #element.css('td:nth-child(5)::text')
     maintainer = scrapy.Field()
+    #element.css('td:nth-child(6) > a::text')
     lastUpdated = scrapy.Field()
+    #element.css('td:nth-child(7)a::text')
